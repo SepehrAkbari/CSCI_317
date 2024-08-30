24090e392f41d99dfd5c7864bb20cc5bf78852fa
Sepehr Akbari <isepehrakbari@gmail.com>
2024-08-30 13:06:25 -0500

    Adding Java Folder
    Project folders,
    Chapter 1

---
+++ src/chapter2/OrderedArray/MyOrderedArray.java
@@ -0,0 +1,20 @@
+package chapter2.OrderedArray;
+import chapter1.*;
+
+public class MyOrderedArray extends chapter1.Array.MyArray {
+    public MyOrderedArray(int size) {
+        super(size);
+    }
+    
+    public int search(int key){
+        return 0;
+    }
+
+    public void add(int item){
+        
+    }
+
+    public int removeKey(){
+        return 0;
+    }
+}
