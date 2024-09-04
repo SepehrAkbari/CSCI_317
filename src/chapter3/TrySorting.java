23b52f5429f437552579c2c09763421241214643
Sepehr Akbari <isepehrakbari@gmail.com>
2024-09-04 14:30:32 -0500

    Update TrySorting.java

--- src/chapter3/TrySorting.java
+++ src/chapter3/TrySorting.java
@@ -32,6 +32,5 @@ public class TrySorting {
 
         System.out.println("\nSorted? " + Sorting.isSorted(array));
         System.out.println("Time taken: " + (endTime - startTime) + "ms");
-        //hello
     }
 }
