25e57547e90a660a354b4efb6c08a01767458801
Sepehr Akbari <isepehrakbari@gmail.com>
2024-09-04 14:25:45 -0500

    9/6

---
+++ src/chapter3/TrySorting.java
@@ -0,0 +1,36 @@
+package chapter3;
+
+import chapter3.Sorting;
+
+public class TrySorting {
+    public static void main(String[] args) {
+        final int SIZE = 100;
+        final int RANGE = 10;
+
+        int[] array = new int[SIZE];
+        
+        for (int i = 0; i < SIZE; i++) {
+            array[i] = (int)(Math.random() * RANGE);
+        }
+
+        System.out.println("Array:");
+        for (int num:array) {
+            System.out.print(num + ", ");
+        }
+        System.out.println("\n");
+
+        long startTime = System.currentTimeMillis();
+        Sorting.bubbleSort(array); // Sorting the array
+        long endTime = System.currentTimeMillis();
+
+        System.out.println("Sorted Array:");
+        for (int num:array) {
+            System.out.print(num + ", ");
+        }
+
+        System.out.println("\n");
+
+        System.out.println("\nSorted? " + Sorting.isSorted(array));
+        System.out.println("Time taken: " + (endTime - startTime) + "ms");
+    }
+}
